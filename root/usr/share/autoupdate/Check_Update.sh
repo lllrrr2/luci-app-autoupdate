4ace0ebe231cc2361c35ff777b1562fb1c0d03e0
Hyy2001X <1804430051@qq.com>
2020-11-07 16:48:20 +0800

    Update Check_Update.sh

--- root/usr/share/autoupdate/Check_Update.sh
+++ root/usr/share/autoupdate/Check_Update.sh
@@ -8,13 +8,19 @@ if [ ! -f /bin/AutoUpdate.sh ];then
 	exit
 fi
 Github=`awk -F '[=]' '/Github/{print $2}' /bin/AutoUpdate.sh | awk 'NR==1'`
+[[ -z "$Github" ]] && exit
 Author=${Github##*com/}
 Github_Tags=https://api.github.com/repos/$Author/releases/latest
 GET_Version=`wget -q $Github_Tags -O - | egrep -o 'R[0-9]+.[0-9]+.[0-9]+.[0-9]+' | awk 'END {print}'`
-if [ -z "$GET_Version" ];then
+CURRENT_Version=$(awk 'NR==1' /etc/openwrt_info)
+if [[ -z "$GET_Version" ]];then
 	echo "未知" > /tmp/cloud_version
 	exit
 else
-	echo "$GET_Version" > /tmp/cloud_version
+	if [[ "$CURRENT_Version" == "$GET_Version" ]];then
+		echo "$GET_Version [最新]" > /tmp/cloud_version
+	else
+		echo "$GET_Version [可更新]" > /tmp/cloud_version
+	fi
 fi
 exit
